2badfd83ebf566992fc5a50d216ac547d8fc9fbe
Santiago Hernández Useche <santiagohu01@gmail.com>
2023-01-10 17:36:03 -0500

    agregando funcion analizarMensaje: Validar que todo sea minúsculas

--- Script.js
+++ Script.js
@@ -5,20 +5,46 @@ const botonEncriptar = document.querySelector(".botonencriptar");
 const botonDesencriptar = document.querySelector(".botondesencriptar");
 const botonCopiar = document.querySelector(".botoncopiar");
 
+function analizarMensaje(){
+  var mensaje = inputTexto.value;
+  var caracteres = "abcdefghijklmnñopqrstuvwxyz";
+  var mensajeErroneo= "";
+  var validador = false;
+
+  for(var caracter of mensaje){
+    if(!caracteres.includes(caracter)){
+      mensajeErroneo += `<p class="parrafoerror"> El caracter ${caracter} no es válido'`
+    }
+  }
+
+  if (mensajeErroneo.length == 0){
+    validador = true;
+  }
+
+  return validador;
+}
+
 function encriptar(){
+  if(analizarMensaje() == false) return;
+
   var mensaje = inputTexto.value;
   var mensajeEncriptado = mensaje.replaceAll("e","enter").replaceAll("i","imes").replaceAll("a","ai").replaceAll("o","ober").replaceAll("u","ufat");
+
   inputEncriptado.value = mensajeEncriptado;
 }
 
 function desencriptar(){
+  if(analizarMensaje() == false) return;
+
   var mensajeEncriptado = inputEncriptado.value;
   var mensajeDesencriptado = mensajeEncriptado.replaceAll("enter","e").replaceAll("imes","i").replaceAll("ai","a").replaceAll("ober","o").replaceAll("ufat","u");
+
   inputTexto.value = mensajeDesencriptado;
 }
 
 function copiar(){
   var mensajeEncriptado = inputEncriptado.value;
+  
   navigator.clipboard.writeText(mensajeEncriptado);
 }
 
